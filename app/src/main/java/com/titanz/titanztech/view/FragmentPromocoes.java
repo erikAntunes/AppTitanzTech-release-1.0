21862b1e5e23bceafb2175c1c40d8579b6aba253
Erik Antunes <erik.antunes@live.com>
2020-01-01 13:58:08 -0300

    pronto para públicar

--- app/src/main/java/com/titanz/titanztech/view/FragmentPromocoes.java
+++ app/src/main/java/com/titanz/titanztech/view/FragmentPromocoes.java
@@ -32,24 +32,49 @@ public class FragmentPromocoes extends Fragment {
         ArrayList<Promocoes> promocoesArrayList = new ArrayList<>();
 
         Promocoes promocoes1 = new Promocoes();
-        promocoes1.setDescricao("Desenvolvimento de Aplicativo Comercial");
+        promocoes1.setDescricao("Aplicativo Android Comercial");
         promocoes1.setValor("R$ 500,00");
         promocoesArrayList.add(promocoes1);
 
         Promocoes promocoes2 = new Promocoes();
         promocoes2.setDescricao("Formatação + Limpeza - Notebook ");
-        promocoes2.setValor("R$ 100,00");
+        promocoes2.setValor("R$ 120,00");
         promocoesArrayList.add(promocoes2);
 
         Promocoes promocoes3 = new Promocoes();
         promocoes3.setDescricao("Formatação + Limpeza - Desktop");
-        promocoes3.setValor("R$ 120,00");
+        promocoes3.setValor("R$ 100,00");
         promocoesArrayList.add(promocoes3);
 
         Promocoes promocoes4 = new Promocoes();
         promocoes4.setDescricao("Formatação + SSD 120GB");
-        promocoes4.setValor("R$ 220,00");
+        promocoes4.setValor("R$ 260,00");
         promocoesArrayList.add(promocoes4);
+
+        Promocoes promocoes5 = new Promocoes();
+        promocoes5.setDescricao("Formatação + SSD 240GB");
+        promocoes5.setValor("R$ 350,00");
+        promocoesArrayList.add(promocoes5);
+
+        Promocoes promocoes6 = new Promocoes();
+        promocoes6.setDescricao("Formatação + SSD 480GB");
+        promocoes6.setValor("R$ 440,00");
+        promocoesArrayList.add(promocoes6);
+
+        Promocoes promocoes7 = new Promocoes();
+        promocoes7.setDescricao("Recuperação de Arquivos Pessoais");
+        promocoes7.setValor("R$ 70,00");
+        promocoesArrayList.add(promocoes7);
+
+        Promocoes promocoes8 = new Promocoes();
+        promocoes8.setDescricao("Limpeza Notebook + Pasta Térmica");
+        promocoes8.setValor("R$ 60,00");
+        promocoesArrayList.add(promocoes8);
+
+        Promocoes promocoes9 = new Promocoes();
+        promocoes9.setDescricao("Limpeza Desktop + Pasta Térmica");
+        promocoes9.setValor("R$ 50,00");
+        promocoesArrayList.add(promocoes9);
 
 
         recyclerViewPromocoes = view.findViewById(R.id.promocoes_recyclerView_id);
