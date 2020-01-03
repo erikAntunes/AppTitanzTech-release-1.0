c5c68dbc112fb0e428fdca525ce5c1bb7184c773
Erik Antunes <erik.antunes@live.com>
2020-01-03 14:29:25 -0300

    funções 99% Ok

--- app/src/main/java/com/titanz/titanztech/view/DestalhesServicosActivity.java
+++ app/src/main/java/com/titanz/titanztech/view/DestalhesServicosActivity.java
@@ -3,9 +3,14 @@ package com.titanz.titanztech.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.titanz.titanztech.R;
@@ -19,6 +24,8 @@ public class DestalhesServicosActivity extends AppCompatActivity {
     private CircleImageView servicoImageView;
     private TextView detalheServicoTextView;
     private TextView nomeDetalheServicoTextViewm;
+    private ImageView whatsappButton;
+    private ImageView homeButtonServicos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +52,24 @@ public class DestalhesServicosActivity extends AppCompatActivity {
         detalheServicoTextView = findViewById(R.id.detalhes_servico_textView);
         nomeDetalheServicoTextViewm = findViewById(R.id.nome_detalhes_servico_textView);
 
+        whatsappButton = findViewById(R.id.whatsapp_button_servicos_imageView);
+        whatsappButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                irParaTitanz();
+            }
+        });
+
+        homeButtonServicos = findViewById(R.id.home_button_servicos_imageView);
+        homeButtonServicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                irParaHome();
+            }
+        });
+
         Intent intent = getIntent();
 
         Bundle bundle = intent.getExtras();
@@ -56,4 +81,41 @@ public class DestalhesServicosActivity extends AppCompatActivity {
         Picasso.get().load(servico.getImagem()).into(servicoImageView);
 
     }
+
+    private void irParaHome() {
+
+        onBackPressed();
+    }
+
+    public void irParaTitanz(){
+
+        String titanz = "+55 11986661870"; // use country code with your phone number
+        String url = "https://api.whatsapp.com/send?phone=" + titanz;
+        try {
+            PackageManager pm = this.getPackageManager();
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+
+            final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+            pDialog.getProgressHelper().setBarColor(R.color.colorPrimary);
+            pDialog.setTitleText("Carregando ...");
+
+            pDialog.setCancelable(true);
+
+            pDialog.show();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    pDialog.dismiss();
+                }
+            },500);
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "... poxa o Whatsapp não está instalado!!!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
 }
